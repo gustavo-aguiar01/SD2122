e6752d048dbcf406ec441275d953721445bd912d
Tomás Tavares <tomastavares21122001@gmail.com>
2022-03-21 22:20:26 +0000

    basic main funcion structure #4

--- Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/Admin.java
+++ Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/Admin.java
@@ -1,5 +1,8 @@
 package pt.ulisboa.tecnico.classes.admin;
 
+import java.util.Scanner;
+import pt.ulisboa.tecnico.classes.admin.AdminFrontend;
+
 public class Admin {
 
   public static void main(String[] args) {
@@ -7,6 +10,20 @@ public class Admin {
     System.out.printf("Received %d Argument(s)%n", args.length);
     for (int i = 0; i < args.length; i++) {
       System.out.printf("args[%d] = %s%n", i, args[i]);
+    }
+
+    final String host = "localhost";
+    final int port = 8080;
+
+    AdminFrontend frontend = new AdminFrontend(host, port);
+
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      System.out.printf("> ");
+      String line = scanner.nextLine();
+
+      System.out.println(line);
     }
   }
 }
---
+++ Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/AdminFrontend.java
@@ -0,0 +1,2 @@
+package pt.ulisboa.tecnico.classes.admin;public class AdminFrontend {
+}
