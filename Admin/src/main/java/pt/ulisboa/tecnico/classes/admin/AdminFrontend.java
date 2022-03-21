4f10576d4c16c2590730f89b904d686fe1984977
Tomás Tavares <tomastavares21122001@gmail.com>
2022-03-21 22:21:07 +0000

    basic frontend structure #4

--- Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/AdminFrontend.java
+++ Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/AdminFrontend.java
@@ -1,2 +1,30 @@
-package pt.ulisboa.tecnico.classes.admin;public class AdminFrontend {
+package pt.ulisboa.tecnico.classes.admin;
+
+import pt.ulisboa.tecnico.classes.contract.admin.AdminServiceGrpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class AdminFrontend {
+
+    private AdminServiceGrpc.AdminServiceBlockingStub stub;
+    private final ManagedChannel channel;
+
+    public AdminFrontend (String host, int port) {
+        this.channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+        this.stub = AdminServiceGrpc.newBlockingStub(this.channel);
+    }
+
+    // admin remote methods
+    public void activate () {
+
+    }
+
+    public void deactivate () {
+
+    }
+
+    public void dump () {
+        // dump method can't be void because it returns a ClassState object
+    }
 }
