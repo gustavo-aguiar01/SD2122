8b04319f7987958fbcca5cebfce70225ace019d0
Tomás Tavares <tomastavares21122001@gmail.com>
2022-03-22 14:30:37 +0000

    basic server service implementation structure #4

--- ClassServer/src/main/java/pt/ulisboa/tecnico/classes/classserver/AdminServiceImpl.java
+++ ClassServer/src/main/java/pt/ulisboa/tecnico/classes/classserver/AdminServiceImpl.java
@@ -1,5 +1,7 @@
 package pt.ulisboa.tecnico.classes.classserver;
 
+import io.grpc.stub.StreamObserver;
+import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer.*;
 import pt.ulisboa.tecnico.classes.contract.admin.AdminServiceGrpc;
 
 public class AdminServiceImpl extends AdminServiceGrpc.AdminServiceImplBase {
@@ -7,5 +9,35 @@ public class AdminServiceImpl extends AdminServiceGrpc.AdminServiceImplBase {
 
     public AdminServiceImpl(Class studentClass) {
         this.studentClass = studentClass;
+    }
+
+    @Override
+    public void activate (ActivateRequest request, StreamObserver<ActivateResponse> responseObserver) {
+
+        System.out.println("Executing activate");
+        ActivateResponse response = ActivateResponse.newBuilder().build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void deactivate (DeactivateRequest request, StreamObserver<DeactivateResponse> responseObserver) {
+
+        System.out.println("Executing deactivate");
+        DeactivateResponse response = DeactivateResponse.newBuilder().build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void dump (DumpRequest request, StreamObserver<DumpResponse> responseObserver) {
+
+        System.out.println("Executing dump");
+        DumpResponse response = DumpResponse.newBuilder().build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
