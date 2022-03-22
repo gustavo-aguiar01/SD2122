e68d9199c695ceb856feae34162ca11cc74f1a87
Tomás Tavares <tomastavares21122001@gmail.com>
2022-03-22 14:12:15 +0000

    admin main loop DONE #4

--- Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/Admin.java
+++ Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/Admin.java
@@ -1,11 +1,21 @@
 package pt.ulisboa.tecnico.classes.admin;
 
 import java.util.Scanner;
-import pt.ulisboa.tecnico.classes.admin.AdminFrontend;
+import java.util.Arrays;
+
+import pt.ulisboa.tecnico.classes.Stringify;
+import pt.ulisboa.tecnico.classes.contract.ClassesDefinitions.ResponseCode;
 
 public class Admin {
 
+  private static final String EXIT_CMD = "exit";
+  private static final String ACTIVATE_CMD = "activate";
+  private static final String DEACTIVATE_CMD = "deactivate";
+  private static final String DUMP_CMD = "dump";
+
   public static void main(String[] args) {
+
+
     System.out.println(Admin.class.getSimpleName());
     System.out.printf("Received %d Argument(s)%n", args.length);
     for (int i = 0; i < args.length; i++) {
@@ -21,9 +31,30 @@ public class Admin {
 
     while (true) {
       System.out.printf("> ");
-      String line = scanner.nextLine();
+      String[] line = scanner.nextLine().split(" ");
+      String command = line[0];
+      String[] arguments = Arrays.copyOfRange(line, 1, line.length);
 
-      System.out.println(line);
+      if (EXIT_CMD.equals(command)) {
+        scanner.close();
+        return;
+
+      } else if (ACTIVATE_CMD.equals(command)) {
+
+        String response = frontend.activate();
+        System.out.println(" - " + response);
+
+      } else if (DEACTIVATE_CMD.equals(command)) {
+
+        String response = frontend.deactivate();
+        System.out.println(" - " + response);
+
+      } else if (DUMP_CMD.equals(command)) {
+
+        String response = frontend.dump();
+        System.out.println(" - " + response);
+
+      }
     }
   }
 }
